2094ce1b109444bb77ee757d86e3563a69840ac5
tanay1337 <tanay1337@gmail.com>
2014-12-03 03:13:56 +0530

    Updated action of Related Searches from IMDb redirection to internal site

--- js/app.js
+++ js/app.js
@@ -1,16 +1,25 @@
+function update(elem) {
+	index = elem.id-1;
+}
+
 function data($scope,$http) {
 	
-	$scope.change = function() { $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
-  .success(function(response) {$scope.list = response;}); 
+  $scope.change = function() { 
+    $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
+     .success(function(response) {$scope.list = response;}); 
 
-  $http.get("http://www.omdbapi.com/?s=" + $scope.search + "&tomatoes=true&plot=full")
-  .success(function(response) {$scope.others = response;});
-};
+    $http.get("http://www.omdbapi.com/?s=" + $scope.search + "&tomatoes=true&plot=full")
+     .success(function(response) {$scope.others = response;});
+  };
   
   $http.get("http://www.omdbapi.com/?t=" + "hackers" + "&tomatoes=true&plot=full")
-  .success(function(response) {$scope.list = response;});
+   .success(function(response) {$scope.list = response;});
 
-  
   $http.get("http://www.omdbapi.com/?s=" + "hackers" + "&tomatoes=true&plot=full")
-  .success(function(response) {$scope.others = response;});
+   .success(function(response) {$scope.others = response;});
+
+  $scope.update = function() {
+    $scope.search = $scope.others.Search[index].Title;
+  	$scope.change();
+  };
 }
