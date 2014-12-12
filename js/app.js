58397f9726141b0aa31cb50cd7f0c28197f319d6
Mayank Tripathi <oquanox@gmail.com>
2014-12-12 21:08:34 +0530

    Removed semicolon from fetch() fixed indentation

--- js/app.js
+++ js/app.js
@@ -1,36 +1,35 @@
-function data($scope,$http) {
-  // when the page loads for the first time 
+function data($scope, $http) {
+  // when the page loads for the first time
   if($scope.search == undefined) {
-    	$scope.search="Sherlock Holmes";
-    	fetch();          
+    $scope.search = "Sherlock Holmes";
+    fetch();
   }
-  
+
   var pendingTask;
 
-  // will load results when the string in search box changes  
+  // will load results when the string in search box changes
   $scope.change = function() {
     if(pendingTask) {
       clearTimeout(pendingTask);
     }
     pendingTask = setTimeout(fetch, 800);
   };
-  
+
   $scope.update = function() {
     $scope.search = $scope.others.Search[index].Title;
   	$scope.change();
   };
-  
-  
+
   function fetch() {
     $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
-     .success(function(response) {$scope.list = response;}); 
+     .success(function(response) {$scope.list = response;});
 
     $http.get("http://www.omdbapi.com/?s=" + $scope.search)
      .success(function(response) {$scope.others = response;});
-  };
+  }
 
 } // end of controller 'data'
 
-  function update(elem) {
-	index = elem.id-1;
-  }
+function update(elem) {
+  index = elem.id - 1;
+}
