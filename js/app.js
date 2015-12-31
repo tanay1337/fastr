tanay1337/fastr

commit d4220bae824a18948e05d3247b289cd99275a1b1

Updated API URL

partials/main-info.html
@@ -3,7 +3,7 @@
 </div>
 
 <div ng-if="details.Response==='True'">
-  <img ng-src="{{ details.Poster=='N/A' ? 'http://placehold.it/150x220&text=N/A' : details.Poster }}" class="thumbnail animated flip movie-poster">
+  <img ng-src="{{ details.Poster=='N/A' ? 'http://placehold.it/150x220&text=N/A' : 'http://imdb.wemakesites.net/api/1.0/img/?url='+details.Poster }}" class="thumbnail animated flip movie-poster">
 
   <span class="span-outer">
     <a href="http://imdb.com/title/{{ details.imdbID }}" target="_blank">{{ details.Title }}</a>

commit aae7b2dbbb99ecfdb944501cff0f6413e4343c51

This commit includes 3 update: 1. Replacing unnecessary setTimeout and clearTimeout calls with ng-model-option debounce feature to update model in a delayed way; and also using $watch service to monitor any model change. I think this increases readability and a lttle bit performance. 2. .success()/.error() has been deprecated, updated to use .then(success_callback, error_callback) instead. 3. Poster image src url has been broken after commit d4220bae824a18948e05d3247b289cd99275a1b1. I tried to invoke with concat("http://imdb.wemakesites.net/api/1.0/img/?url=", details.Poster), however I got: {"status":"fail","code":1,"message":"IMDb ID not set"}. Actually "details.Poster" has already contained the img src url.
Signed-off-by: jonnyhsy <jonnyhsy@foxmail.com>

index.html
@@ -25,7 +25,7 @@
 
       <div class="animated zoomInRight">
         <div class="input-group search-bar">
-          <input type="text" ng-model="search" ng-change="change()" onclick="select()" class="form-control" placeholder="Enter full movie name" autofocus />
+          <input type="text" ng-model="search" ng-model-options="{ debounce: 800 }" onclick="select()" class="form-control" placeholder="Enter full movie name" autofocus />
           <span class="input-group-addon bar-style"><i class="glyphicon glyphicon-search"></i></span>
         </div>
 

js/app.js
@@ -2,31 +2,23 @@
 
 angular.module('myApp', [])
   .controller('MovieController', function($scope, $http){
-    var pendingTask;
+    $scope.search = "Sherlock Holmes";
 
-    if($scope.search === undefined){
-      $scope.search = "Sherlock Holmes";
+    $scope.$watch('search', function() {
       fetch();
-    }
-
-    $scope.change = function(){
-      if(pendingTask){
-        clearTimeout(pendingTask);
-      }
-      pendingTask = setTimeout(fetch, 800);
-    };
+    });
 
     function fetch(){
       $http.get("http://www.omdbapi.com/?t=" + $scope.search + "&tomatoes=true&plot=full")
-       .success(function(response){ $scope.details = response; });
+      .then(function(response){ $scope.details = response.data; });
 
       $http.get("http://www.omdbapi.com/?s=" + $scope.search)
-       .success(function(response){  $scope.related = response; });
+      .then(function(response){ $scope.related = response.data; });
     }
 
     $scope.update = function(movie){
       $scope.search = movie.Title;
-      $scope.change();
+      fetch();
     };
 
     $scope.select = function(){

partials/main-info.html
@@ -3,7 +3,7 @@
 </div>
 
 <div ng-if="details.Response==='True'">
-  <img ng-src="{{ details.Poster=='N/A' ? 'http://placehold.it/150x220&text=N/A' : 'http://imdb.wemakesites.net/api/1.0/img/?url='+details.Poster }}" class="thumbnail animated flip movie-poster">
+  <img ng-src="{{ details.Poster=='N/A' ? 'http://placehold.it/150x220&text=N/A' : details.Poster }}" class="thumbnail animated flip movie-poster">
 
   <span class="span-outer">
     <a href="http://imdb.com/title/{{ details.imdbID }}" target="_blank">{{ details.Title }}</a>